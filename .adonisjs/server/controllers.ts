c61db358b060b6520425d20834160aa278af4357
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-09-02 02:10:37 -0500

    created home controller

--- .adonisjs/server/controllers.ts
+++ .adonisjs/server/controllers.ts
@@ -4,6 +4,8 @@
  */
 
 export const controllers = {
+  Home: () => import('#controllers/home_controller'),
   NewAccount: () => import('#controllers/new_account_controller'),
   Session: () => import('#controllers/session_controller'),
+  Users: () => import('#controllers/users_controller'),
 }

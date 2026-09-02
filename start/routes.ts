5ceede189e2e8fe22f8e6c843d6c25aab3547e36
Santiago Idárraga Ceballos <san200526@gmail.com>
2026-09-02 02:11:19 -0500

    changed home route definition

--- start/routes.ts
+++ start/routes.ts
@@ -11,7 +11,7 @@ import { middleware } from '#start/kernel'
 import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home').as('home')
+router.get('/', [controllers.Home, 'index']).as('home')
 
 router
   .group(() => {
